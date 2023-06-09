21d7cc42c72a2fb674ae74168f919b73acbbc1cb
TKuhner <trevorkuhner29@gmail.com>
2023-06-09 07:51:28 -0500

    added generateMarkdown, need to test

--- assets/utils/generateMarkdown.js
+++ assets/utils/generateMarkdown.js
@@ -1,5 +1,39 @@
 function generateMarkdown({ title, description, installation, usage, license, contributing, tests, questions }) {
 
+    return `# ${title}
+${badge[license]}
+
+## Description
+${description}  
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [License](#license)
+
+## Installation
+${installation} 
+
+## Usage
+${usage}
+
+## Contributing
+${contributing}
+
+## Tests
+${tests}
+
+## Questions
+Contact email: ${email}
+GitHub: ${github})
+${questions}
+
+## License
+This project is licensed under the terms of the ${license} license.
+`;
 }
 
 module.exports = generateMarkdown;
